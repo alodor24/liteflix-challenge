b8f4159082525c2b6e6969bbe2bc8313714b13f4
alodor24 <jose-a-mendez@hotmail.com>
2021-10-01 16:49:35 -0300

    Get data of API

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
 import AppLayout from '../components/AppLayout'
 import Hero from '../components/Hero'
 
-const Home = () => {
+const Home = (props) => {
+    const data = props.data
+    const films = data.slice(0, 4)
+
     return (
         <AppLayout
         description='Liteflix is ​​a new movie and video streaming app.'
         title='Liteflix | Home'
         >
             <Hero />
+            { 
+                films.map(item => {
+                    console.log(item)
+                })
+            }
         </AppLayout>
     )
+}
+
+export async function getServerSideProps(context) {
+    const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=6f26fd536dd6192ec8a57e94141f8b20')
+    const resJSON = await res.json()
+
+    return {
+      props: {
+        data: resJSON.results
+      }
+    }
 }
 
 export default Home
