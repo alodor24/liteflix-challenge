bb81b4847b8646dfe56c3ae9344a61746053a9b6
alodor24 <jose-a-mendez@hotmail.com>
2021-10-04 12:14:08 -0300

    Adjusted dropdown section

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -6,34 +6,75 @@ import MyMovies from './MyMovies'
 const Sidebar = (props) => {
     const [showPopularFilms, setShowPopularFilms] = useState(true)
     const [showMyMovies, setShowMyMovies] = useState(false)
-    const [titleDropdown, setTitleDropdown] = useState('PELÍCULAS')
+    const [titleDropdown, setTitleDropdown] = useState('POPULARES')
+    const [showDropdown, setShowDropdown] = useState(false)
+
+    const [showCheckPopularFilms, setShowCheckPopularFilms] = useState(true)
+    const [showCheckMyMovies, setShowCheckMyMovies] = useState(false)
 
     const handleShowPopularFilms = () => {
         setShowPopularFilms(true)
         setShowMyMovies(false)
-        setTitleDropdown('PELÍCULAS')
+        setTitleDropdown('POPULARES')
+        setShowDropdown(false)
+        setShowCheckMyMovies(false)
+
+        if (!showCheckPopularFilms) {
+            setShowCheckPopularFilms(true)
+        } else {
+            setShowCheckPopularFilms(false)
+        }
     }
 
     const handleShowMyMovies = () => {
         setShowPopularFilms(false)
         setShowMyMovies(true)
         setTitleDropdown('MIS PELÍCULAS')
+        setShowDropdown(false)
+        setShowCheckPopularFilms(false)
+
+        if (!showCheckMyMovies) {
+            setShowCheckMyMovies(true)
+        } else {
+            setShowCheckMyMovies(false)
+        }
+    }
+
+    const handleDropdown = () => {
+        if (!showDropdown) {
+            setShowDropdown(true)
+        } else {
+            setShowDropdown(false)
+        }
     }
 
     return (
         <Container className="p-3">
             <Row className="mt-4 mb-4">
-                <Dropdown className="wow zoomIn">
-                    <Dropdown.Toggle id="dropdown-basic" className="dropdown-movies">
+                <div className="dropdown-section wow zoomIn">
+                    <div className="dropdown-movies" onClick={ handleDropdown }>
                         <span className="dropdown-title-left">VER: </span> 
-                        <span className="dropdown-title-right">{ titleDropdown }</span>
-                    </Dropdown.Toggle>
+                        <span className="dropdown-title-right">{ titleDropdown } </span>
+                        <span className="dropdown-chevron-down">
+                            <svg width="13" height="8" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 1L6.54557 6.54557L12.0911 1" stroke="white" strokeWidth="2"/>
+                            </svg>
+                        </span>
+                    </div>
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item onClick={ handleShowPopularFilms }>POPULARES</Dropdown.Item>
-                        <Dropdown.Item onClick={ handleShowMyMovies }>MIS PELÍCULAS</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
+                    <ul className={ 'dropdown-options ' + (showDropdown ? 'active' : '') }>
+                        <li 
+                        className={ showCheckPopularFilms ? 'active' : '' }
+                        onClick={ handleShowPopularFilms }>
+                            POPULARES
+                        </li>
+                        <li 
+                        className={ showCheckMyMovies ? 'active' : '' }
+                        onClick={ handleShowMyMovies }>
+                            MIS PELÍCULAS
+                        </li>
+                    </ul>
+                </div>
             </Row>
 
             <div className={ 'popular-movies ' + (showPopularFilms ? 'active' : '') }>
