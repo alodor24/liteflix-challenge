10ec35b7e56126ee02ea198eb7e72fe9ca5715ac
alodor24 <jose-a-mendez@hotmail.com>
2021-10-05 22:19:30 -0300

    Show data from localstorage

--- components/MyMovies.jsx
+++ components/MyMovies.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Image from 'next/image'
+import { Row } from 'react-bootstrap'
 
 const MyMovies = () => {
 
@@ -7,29 +8,50 @@ const MyMovies = () => {
     const [showData, setShowData] = useState(false)
 
     useEffect(() => {
-        
         if (localStorage.getItem('dataMovie') !== null) {
 
             const getData = localStorage.getItem('dataMovie')
-            let dataMovieLocalStorage = JSON.parse(getData)
-            
-            setDataLocalStorage(dataMovieLocalStorage)
-            setShowData(true)
-            console.log(dataLocalStorage)
+            const dataMovieLocalStorage = JSON.parse(getData)
+
+            setDataLocalStorage([dataMovieLocalStorage])
+            //console.log(dataLocalStorage)
         }
+        setShowData(true)
+
     }, [showData])
 
     const dataDone = () => {
         if (showData) {
             return (
                 <>
-                    {/*
-                    <Image 
-                    className="film-image"
-                    src={ imageLocalstorage }
-                    alt=""
-                    layout='fill'/> */}
-                    <p>Aquí estoy</p>
+                    {
+                        dataLocalStorage.map((movie, index) => (
+                            <Row key={ index } className="mb-4 wow zoomIn">
+                                <article className="film-container">
+                                    <Image 
+                                    className="film-image"
+                                    src={ movie.image }
+                                    alt={ movie.title }
+                                    layout='fill'
+                                    />
+
+                                    <div className="front-side">
+                                        <span className="front-side-icon">
+                                            <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <circle cx="20" cy="20" r="19.5" fill="#242424" fillOpacity="0.5" stroke="white"/>
+                                                <path fillRule="evenodd" clipRule="evenodd" d="M26.6484 20.2701L16 13V27L26.6484 20.2701Z" stroke="white"/>
+                                            </svg>
+                                        </span>
+
+                                        <h6 className="front-side-title">
+                                            { movie.title }
+                                        </h6>
+                                    </div>
+                                    <div className="back-side"></div>
+                                </article>
+                            </Row>
+                        ))
+                    }
                 </>
             )
         }
@@ -37,11 +59,7 @@ const MyMovies = () => {
 
     return (
         <>
-            <h3 style={{ color: 'red', }}>Mis Películas</h3>
-
-            <div>
-                { dataDone() }
-            </div>
+            { dataDone() }
         </>
     )
 }
